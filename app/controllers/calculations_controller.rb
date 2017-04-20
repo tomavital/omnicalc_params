6155e263f9b744db8a6ec41ac01a5ff8f92b309d
rails -v <c:sites>
2017-04-20 15:34:32 -0500

    root

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -4,4 +4,9 @@ class CalculationsController <ApplicationController
     @square = @user_number **2
     render("calculations/flex_square_5.html.erb")
   end
+  def flex_square_root_5
+    @user_number = params["num"].to_f
+    @square = @user_number **0.5
+    render("calculations/flex_square_root_5.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_5"})
+  get("/flexible/square_root/:num", {:controller => "calculations", :action => "flex_square_root_5"})
 end
