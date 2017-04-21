b00632359e871d83d40b7de1d896eeec6d06b634
rails -v <c:sites>
2017-04-20 19:48:43 -0500

    tom

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -28,12 +28,15 @@ class CalculationsController <ApplicationController
     @square = rand(@user_max-@user_min)+@user_min
     render("calculations/flex_random_5.html.erb")
   end
-  def square
-    @user_number = params[:user_number].to_f
-    @square = @user_number **2
-    render("calculations/square.html.erb")
-  end
+
   def square_form
     render("calculations/square_form.html.erb")
   end
+  
+  def square
+  @user_number = params["user_number"].to_f
+  @square = @user_number**2
+  render ("calculations/square_results.html.erb")
+end
+
 end
